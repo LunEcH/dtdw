deaa0958491ba3b0d8ea9d177bd95f9f20cc6dd9
lunech <70895350+LunEcH@users.noreply.github.com>
2025-07-06 01:15:00 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/dtdw/', // 👈 BU ÇOK ÖNEMLİ!
-  plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
-})
+  base: '/dtdw/',  
+  plugi
